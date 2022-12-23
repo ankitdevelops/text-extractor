59decbdd85c0b45c25f600f64d74dfe4757b6dfc
Ankit Kumar <heyankitkr@gmail.com>
2022-12-23 18:37:57 +0530

    db to postgres

--- coreProj/settings.py
+++ coreProj/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
-    "allauth.socialaccount.providers.google",
     "allauth.socialaccount.providers.github",
 ]
 
@@ -85,10 +84,20 @@ WSGI_APPLICATION = "coreProj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "text",
+        "USER": "postgres",
+        "PASSWORD": "ankit93",
+        "HOST": "localhost",
     }
 }
 
